c1b8b036169268ddda87b14f5308d51462eb1e7d
David Lee <Dai180293@gmail.com>
2023-05-23 19:29:47 +0100

    removed chakra ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { MantineProvider } from "@mantine/core";
 // import { store } from "./app/store.js";
 // import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <MantineProvider>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </MantineProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
