7da94ec08883c9c9824a459edf4091649da14ed4
David Lee <Dai180293@gmail.com>
2023-05-23 09:27:01 +0100

    removed jsx from onboarding page

--- frontend/src/components/pages/Onboarding.jsx
+++ frontend/src/components/pages/Onboarding.jsx
@@ -1,19 +1,7 @@
-import { Link } from "react-router-dom";
-
 const Onboarding = () => {
     return (
         <section className="onboarding_bg_container">
             <section className="onboarding_sign-in_options">
-                <h1>Welcome to UpTo</h1>
-                <p>Improve your productivity by managing your daily tasks efficiently</p>
-                <section className="sign-in_links">
-                    <Link to="/login" className="login_cta">
-                        LOGIN
-                    </Link>
-                    <Link to="/register" className="register_cta">
-                        REGISTER
-                    </Link>
-                </section>
             </section>
         </section>
     );
