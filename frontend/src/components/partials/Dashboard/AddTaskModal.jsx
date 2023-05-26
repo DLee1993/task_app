8fb8f570b5007a65357155e0a1dde92c2f247545
David Lee <Dai180293@gmail.com>
2023-05-26 01:44:10 +0100

    added the add task modal

--- frontend/src/components/partials/Dashboard/AddTaskModal.jsx
+++ frontend/src/components/partials/Dashboard/AddTaskModal.jsx
@@ -1,41 +1,95 @@
-/* eslint-disable react/prop-types */
 import { useState } from "react";
-import { Button, Modal } from "@mantine/core";
+import { PropTypes } from "prop-types";
+import { Button, Modal, Group } from "@mantine/core";
+import { DatePicker } from "@mantine/dates";
 
 const AddTaskModal = ({ taskMenuOpened, close }) => {
     const [taskTitle, setTaskTitle] = useState("");
+    const [date, setDate] = useState(null);
     const [taskDescription, setTaskDescription] = useState("");
+    const [categoryValue, setCategoryValue] = useState("home");
+
+    const canSave = [taskTitle, taskDescription, categoryValue].every(Boolean);
+
+    const formCancelled = () => {
+        setTaskTitle("");
+        setTaskDescription("");
+        setDate(null);
+        setCategoryValue("home");
+        close(true)
+    };
+    
+    const formSubmitted = (e) => {
+        e.preventDefault();
+        setTaskTitle("");
+        setTaskDescription("");
+        setDate(null);
+        setCategoryValue("home");
+    };
 
     return (
-        <Modal opened={taskMenuOpened} onClose={close} title="Add a Task">
-            <form className="addTaskForm">
-                <fieldset className="task_title">
-                    <label htmlFor="task_title">Title: </label>
-                    <input
-                        type="text"
-                        id="task_title"
-                        value={taskTitle}
-                        onChange={(e) => setTaskTitle(e.target.value)}
-                    />
-                </fieldset>
-                <fieldset className="task_description">
-                    <label htmlFor="task_description">Description: </label>
-                    <textarea
-                        type="text"
-                        id="task_description"
-                        value={taskDescription}
-                        onChange={(e) => setTaskDescription(e.target.value)}
-                    />
-                </fieldset>
-                <Button.Group>
-                    <Button className="cancelTask-cta" onClick={close}>
-                        Cancel
-                    </Button>
-                    <Button className="nextInput-cta">Next</Button>
-                </Button.Group>
-            </form>
-        </Modal>
+        <>
+            <Modal opened={taskMenuOpened} onClose={close} title="Add a Task" fullScreen>
+                <form className="addTaskForm" onSubmit={formSubmitted}>
+                    <fieldset className="task_details">
+                        <label htmlFor="task_title">Title: </label>
+                        <input
+                            type="text"
+                            id="task_title"
+                            value={taskTitle}
+                            onChange={(e) => setTaskTitle(e.target.value)}
+                        />
+                        <label htmlFor="task_description">Description: </label>
+                        <textarea
+                            type="text"
+                            id="task_description"
+                            value={taskDescription}
+                            onChange={(e) => setTaskDescription(e.target.value)}
+                        />
+                        <label htmlFor="task_category">Select a Category:</label>
+                        <select
+                            name="category"
+                            id="task_category"
+                            value={categoryValue}
+                            onChange={(e) => setCategoryValue(e.target.value)}
+                        >
+                            <option value="home">Home</option>
+                            <option value="university">University</option>
+                            <option value="gym">Gym</option>
+                            <option value="groceries">Groceries</option>
+                            <option value="work">Work</option>
+                            <option value="movies">Movies</option>
+                            <option value="music">Music</option>
+                        </select>
+                    </fieldset>
+                    <fieldset className="date_select">
+                        <h5>Pick a Date to complete the task by ( optional )</h5>
+                        <Group position="center">
+                            <DatePicker allowDeselect value={date} onChange={setDate} size="md" />
+                        </Group>
+                    </fieldset>
+                    <Button.Group>
+                        <Button className="cancel-cta" onClick={formCancelled}>
+                            Cancel
+                        </Button>
+                        <Button
+                            className="submit-cta"
+                            type="submit"
+                            onClick={close}
+                            disabled={!canSave}
+                        >
+                            Save
+                        </Button>
+                    </Button.Group>
+                </form>
+            </Modal>
+        </>
     );
+};
+
+AddTaskModal.propTypes = {
+    taskMenuOpened: PropTypes.bool,
+    close: PropTypes.func,
 };
 
 export default AddTaskModal;
