e73077268703ad7b0c9ace0cb3f60394beffe904
David Lee <Dai180293@gmail.com>
2023-05-31 11:16:44 +0100

    re-structured the app to have all of the modals in the main dashboard component and pass down the state needed to open/close modals

--- frontend/src/components/pages/Dashboard.jsx
+++ frontend/src/components/pages/Dashboard.jsx
@@ -1,10 +1,21 @@
 import Header from "../partials/Dashboard/Header";
 import TasksList from "../../appFeatures/TasksList";
+import { useDisclosure } from "@mantine/hooks";
+import AddTaskModal from "../partials/Dashboard/AddTaskModal";
+import ConfirmAccountLogout from "../partials/Dashboard/ConfirmAccountLogout";
 
 const Dashboard = () => {
+    const [taskMenuOpened, { open: taskMenuOpen, close: taskMenuClose }] = useDisclosure(false);
+    const [logoutAccountOpened, { open: logoutAccountOpen, close: logoutAccountClose }] =
+        useDisclosure(false);
     return (
         <main className="dashboard">
-            <Header />
+            <Header taskMenuOpen={taskMenuOpen} logoutAccountOpen={logoutAccountOpen} />
+            <AddTaskModal taskMenuOpened={taskMenuOpened} close={taskMenuClose} />
+            <ConfirmAccountLogout
+                logoutAccountOpened={logoutAccountOpened}
+                close={logoutAccountClose}
+            />
             <section className="main">
                 <TasksList />
             </section>
--- frontend/src/components/partials/Dashboard/Header.jsx
+++ frontend/src/components/partials/Dashboard/Header.jsx
@@ -1,27 +1,18 @@
-import { useDisclosure } from "@mantine/hooks";
+import { PropTypes } from "prop-types";
 import NavIconBar from "./NavIconBar";
-import AddTaskModal from "./AddTaskModal";
-import ConfirmAccountLogout from "./ConfirmAccountLogout";
 
-//- icon needs to add taskmenuopened when clicked
-//- same goes for delete/log out icon
-
-const Header = () => {
-    const [taskMenuOpened, { open: taskMenuOpen, close: taskMenuClose }] = useDisclosure(false);
-    const [logoutAccountOpened, { open: logoutAccountOpen, close: logoutAccountClose }] =
-        useDisclosure(false);
-
+const Header = ({ taskMenuOpen, logoutAccountOpen }) => {
     return (
         <header className="app_header">
             <h3>Welcome User</h3>
             <NavIconBar taskMenuOpen={taskMenuOpen} logoutAccountOpen={logoutAccountOpen} />
-            <AddTaskModal taskMenuOpened={taskMenuOpened} close={taskMenuClose} />
-            <ConfirmAccountLogout
-                logoutAccountOpened={logoutAccountOpened}
-                close={logoutAccountClose}
-            />
         </header>
     );
+};
+
+Header.propTypes = {
+    taskMenuOpen: PropTypes.func,
+    logoutAccountOpen: PropTypes.func,
 };
 
 export default Header;
