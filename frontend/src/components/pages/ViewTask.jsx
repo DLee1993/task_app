7c60be16cd456f879d8053a59abfb82819d0f73e
David Lee <Dai180293@gmail.com>
2023-06-15 15:37:57 +0100

    added functionality to delete a task

--- frontend/src/components/pages/ViewTask.jsx
+++ frontend/src/components/pages/ViewTask.jsx
@@ -1,8 +1,10 @@
+import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Button } from "@mantine/core";
 import { IoArrowBack } from "react-icons/io5";
-import { selectTaskById } from "../../appFeatures/tasks/tasksSlice";
+import { selectTaskById, useDeleteTaskMutation } from "../../appFeatures/tasks/tasksSlice";
 import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 const ViewTask = () => {
     const navigate = useNavigate();
@@ -11,10 +13,26 @@ const ViewTask = () => {
     const createdAt = task.createdAt.slice(0, 10);
     const updatedAt = task.updatedAt.slice(0, 10);
 
-    console.log(new Date().toISOString());
+    const [deleteTask, { isSuccess, isError, error }] = useDeleteTaskMutation();
+
+    useEffect(() => {
+        if (isSuccess) {
+            navigate("/dashboard");
+        }
+
+        if (isError) {
+            toast.error(`${error.data.message}`);
+        }
+    }, [isSuccess, isError, error, navigate]);
 
     const handleEditRedirect = () => {
-        navigate(`/dashboard`);
+        navigate(`/dashboard/edit/${id}`);
+    };
+
+    const onDeleteTaskClicked = async () => {
+        await deleteTask({ id: task.id });
+        navigate("/dashboard");
+        window.location.reload();
     };
 
     return (
@@ -34,7 +52,9 @@ const ViewTask = () => {
                     Last updated: <span>{updatedAt.split("-").reverse().join("-")}</span>
                 </p>
                 <section className="task_actions">
-                    <Button className="warning color_tranisiton">Delete Task</Button>
+                    <Button className="warning color_tranisiton" onClick={onDeleteTaskClicked}>
+                        Delete Task
+                    </Button>
                     <Button className="filled color_tranisiton" onClick={handleEditRedirect}>
                         Update Task
                     </Button>
