768360be8583f8d26a6343f7e6b514f19bf0c26f
David Lee <Dai180293@gmail.com>
2023-06-16 18:18:42 +0100

    adjusted the task table column widths

--- frontend/src/appFeatures/tasks/Task.jsx
+++ frontend/src/appFeatures/tasks/Task.jsx
@@ -7,14 +7,24 @@ import { FaCheck } from "react-icons/fa";
 
 const Task = ({ taskId }) => {
     const task = useSelector((state) => selectTaskById(state, taskId));
+    let titleContent;
+    let descriptionContent;
+
+    task.task_description.length > 30
+        ? (descriptionContent = `${task.task_description.substring(0, 30)}...`)
+        : (descriptionContent = task.task_description);
+
+    task.task_title.length > 30
+        ? (titleContent = `${task.task_title.substring(0, 30)}...`)
+        : (titleContent = task.task_title);
 
     return (
         <tr>
             <td className="title">
-                <Text>{task.task_title}</Text>
+                <Text>{titleContent}</Text>
             </td>
             <td className="description">
-                <Text>{task.task_description}</Text>
+                <Text>{descriptionContent}</Text>
             </td>
             <td className="category">{task.category}</td>
             <td className="completed">{task.completed ? <FaCheck /> : null}</td>
