61eeaefc5f65056ff6cd20ed2c1836955c960b1e
David Lee <Dai180293@gmail.com>
2023-07-02 17:44:29 +0100

    added animation to onboarding page

--- frontend/src/components/pages/Onboarding.jsx
+++ frontend/src/components/pages/Onboarding.jsx
@@ -18,17 +18,17 @@ const Onboarding = () => {
         >
             <section id="onboarding_message">
                 <h1
-                    className="text-3xl sm:text-4xl md:text-5xl max-w-prose"
+                    className="text-3xl sm:text-4xl md:text-5xl max-w-prose opacity-0 translate-y-5"
                     ref={(el) => (onboardingHeader = el)}
                 >
                     Welcome to Upto
                 </h1>
-                <p className="max-w-md my-10" ref={(el) => (onboardingText = el)}>
+                <p className="max-w-md my-10 opacity-0 translate-y-5" ref={(el) => (onboardingText = el)}>
                     Utilise our software to organise your daily tasks and increase your productivity
                 </p>
             </section>
             <section
-                className="w-80 h-28 flex flex-col justify-evenly items-center"
+                className="w-80 h-28 flex flex-col justify-evenly items-center opacity-0 translate-y-5"
                 id="onboarding_sign-in_options"
                 ref={(el) => (onboardingSignInLinks = el)}
             >
