43cd3e8617f8cda30caac266c39aa14583338765
David Lee <Dai180293@gmail.com>
2023-07-03 17:23:12 +0100

    styling for the add task page completed

--- frontend/src/components/pages/AddTask.jsx
+++ frontend/src/components/pages/AddTask.jsx
@@ -46,38 +46,54 @@ const AddTask = () => {
 
     return (
         <>
-            <Header title={"Add a new Task"}/>
-            <section className="add_task_form_container">
-                <form className="addTaskForm" onSubmit={onSaveTaskClicked}>
-                    <section>
-                        <fieldset className="task_title">
-                            <label htmlFor="task_title">Title:</label>
+            <Header title={"Add a new Task"} />
+            <section
+                id="add_task_form_container"
+                className="flex justify-center items-center flex-col h-screen sm:h-full min-h-[650px] sm:min-h-0 sm:mt-10"
+            >
+                <form
+                    id="addTaskForm"
+                    onSubmit={onSaveTaskClicked}
+                    className="w-full flex justify-evenly items-center flex-col sm:flex-row"
+                >
+                    <section className="sm:min-h-[350px] flex justify-between items-center flex-col">
+                        <fieldset id="task_title_input" className="flex flex-col">
+                            <label htmlFor="task_title" className="text-lg pb-2">
+                                Title:
+                            </label>
                             <input
                                 type="text"
                                 id="task_title"
                                 value={title}
                                 onChange={(e) => setTitle(e.target.value)}
                                 autoFocus
+                                className="bg-fadedBlack w-80 h-10 text-fadedWhite pl-1"
                             />
                         </fieldset>
-                        <fieldset className="task_description">
-                            <label htmlFor="task_description">Description:</label>
+                        <fieldset id="task_description_input" className="flex flex-col">
+                            <label htmlFor="task_description" className="text-lg pb-2">
+                                Description:
+                            </label>
                             <textarea
                                 type="text"
                                 id="task_description"
                                 value={description}
                                 onChange={(e) => setDescription(e.target.value)}
+                                className="bg-fadedBlack w-80 h-52 text-fadedWhite pl-1 resize-none"
                             />
                         </fieldset>
                     </section>
-                    <section>
-                        <fieldset className="task_category">
-                            <label htmlFor="task_category">Pick a category for your task:</label>
+                    <section className="sm:min-h-[350px] flex justify-between items-end flex-col mb-14 sm:mb-0">
+                        <fieldset id="task_category_input" className="flex flex-col">
+                            <label htmlFor="task_category" className="text-lg pb-2">
+                                Task Category: ( optional )
+                            </label>
                             <select
                                 name="task_category"
                                 id="task_category"
                                 value={category}
                                 onChange={(e) => setCategory(e.target.value)}
+                                className="bg-fadedBlack w-80 h-10 text-fadedWhite pl-1"
                             >
                                 <option value="home">Home</option>
                                 <option value="work">Work</option>
@@ -87,12 +103,13 @@ const AddTask = () => {
                                 <option value="health">Health</option>
                             </select>
                         </fieldset>
-                        <section className="form_btn_group">
+                        <section id="form_btn_group" className="w-full my-10 sm:my-0">
                             <button
                                 type="submit"
-                                className="form_btn filled color_transition"
+                                id="form_btn filled color_transition"
                                 aria-label="submit new task button"
                                 disabled={!canSave}
+                                className="w-full cursor-pointer sm:max-w-xs h-10 sm:h-12 text-lg flex justify-center items-center bg-transparent border-brightPurple border-2 hover:bg-brightPurple hover:border-transparent transition duration-200"
                             >
                                 Submit
                             </button>
