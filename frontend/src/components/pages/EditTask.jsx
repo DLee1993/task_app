fa44fcf85b9fdaf1843544f6e4b865e6b213c7c8
David Lee <Dai180293@gmail.com>
2023-07-03 18:34:20 +0100

    edit task page styling completed

--- frontend/src/components/pages/EditTask.jsx
+++ frontend/src/components/pages/EditTask.jsx
@@ -87,32 +87,45 @@ const EditTask = () => {
     return (
         <>
             <Header title={"Edit a Task"} />
-            <section className="edit_task_form_container">
-                <form className="editTaskForm" onSubmit={onSaveTaskClicked}>
-                    <section>
-                        <fieldset className="task_title">
-                            <label htmlFor="editTask_title">Title:</label>
+            <section
+                id="edit_task_form_container"
+                className="flex justify-center items-center flex-col h-screen sm:h-full min-h-[650px] sm:min-h-0 sm:mt-10"
+            >
+                <form
+                    id="editTaskForm"
+                    onSubmit={onSaveTaskClicked}
+                    className="w-full flex justify-evenly items-center flex-col sm:flex-row"
+                >
+                    <section className="sm:min-h-[350px] flex justify-between items-center flex-col">
+                        <fieldset id="task_title" className="flex flex-col">
+                            <label htmlFor="editTask_title" className="text-lg pb-2">
+                                Title:
+                            </label>
                             <input
                                 type="text"
                                 id="editTask_title"
                                 value={title}
                                 onChange={(e) => setTitle(e.target.value)}
                                 autoFocus
+                                className="bg-fadedBlack w-80 h-10 text-fadedWhite pl-1"
                             />
                         </fieldset>
-                        <fieldset className="task_description">
-                            <label htmlFor="editTask_description">Description:</label>
+                        <fieldset id="task_description" className="flex flex-col">
+                            <label htmlFor="editTask_description" className="text-lg pb-2">
+                                Description:
+                            </label>
                             <textarea
                                 type="text"
                                 id="editTask_description"
                                 value={description}
                                 onChange={(e) => setDescription(e.target.value)}
+                                className="bg-fadedBlack w-80 h-10 text-fadedWhite pl-1"
                             />
                         </fieldset>
                     </section>
-                    <section>
-                        <fieldset className="task_category">
-                            <label htmlFor="editTask_category">
+                    <section className="sm:min-h-[350px] flex justify-between items-end flex-col mb-14 sm:mb-0">
+                        <fieldset id="task_category" className="flex flex-col">
+                            <label htmlFor="editTask_category" className="text-lg pb-2">
                                 Pick a category for your task:
                             </label>
                             <select
@@ -120,6 +133,7 @@ const EditTask = () => {
                                 id="editTask_category"
                                 value={category}
                                 onChange={(e) => setCategory(e.target.value)}
+                                className="bg-fadedBlack w-80 h-10 text-fadedWhite pl-1"
                             >
                                 <option value="home">Home</option>
                                 <option value="work">Work</option>
@@ -129,27 +143,34 @@ const EditTask = () => {
                                 <option value="health">Health</option>
                             </select>
                         </fieldset>
-                        <section className="form_btn_group">
+                        <fieldset className="w-full flex justify-between items-center">
+                            <label htmlFor="completedStatus" className="text-lg pb-2 h-5">
+                                Mark as {completed ? "incompleted" : "complete"}
+                            </label>
+                            <input
+                                type="checkbox"
+                                name="completed"
+                                id="completedStatus"
+                                aria-label="check task"
+                                onClick={checkMark}
+                                className="w-5 h-5"
+                            />
+                        </fieldset>
+                        <section id="form_btn_group" className="w-full my-10 sm:my-0">
                             <button
                                 type="submit"
-                                className="form_btn filled color_transition"
+                                id="form_btn filled color_transition"
                                 aria-label="submit updated task button"
+                                className="w-full mb-2 cursor-pointer sm:max-w-xs h-10 sm:h-12 text-lg flex justify-center items-center bg-fadedPurple hover:bg-brightPurple transition duration-200"
                             >
                                 Save Task
                             </button>
                             <button
                                 type="button"
-                                className="form_btn outline color_transition"
-                                onClick={checkMark}
-                                aria-label="check task button"
-                            >
-                                {!completed ? "Mark as complete" : "Mark as incomplete"}
-                            </button>
-                            <button
-                                type="button"
                                 onClick={onDeleteTaskClicked}
-                                className="form_btn warning color_transition"
+                                id="form_btn warning color_transition"
                                 aria-label="delete task button"
+                                className="w-full cursor-pointer sm:max-w-xs h-10 sm:h-12 text-lg flex justify-center items-center bg-fadedWarningRed hover:bg-warningRed transition duration-200"
                             >
                                 Delete Task
                             </button>
