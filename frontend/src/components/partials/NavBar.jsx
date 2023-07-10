066a1d8ef4f7f5a57d740fa811a68fdb2ddfaa70
David Lee <Dai180293@gmail.com>
2023-07-10 19:04:37 +0100

    added a dynamic navbar, content will change showing only links to pages the user is not currently on

--- frontend/src/components/partials/NavBar.jsx
+++ frontend/src/components/partials/NavBar.jsx
@@ -1,11 +1,16 @@
 import { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useSendLogoutMutation } from "../../appFeatures/auth/authApiSlice";
 import { toast } from "react-toastify";
 
 const NavBar = () => {
+    let dashboardLink = null;
+    let profileLink = null;
+    let addTaskLink = null;
     const navigate = useNavigate();
+    const { pathname } = useLocation();
     const [sendLogout, { isloading, isSuccess, isError, error }] = useSendLogoutMutation();
+    console.log(pathname)
 
     useEffect(() => {
         if (isSuccess) navigate("/");
@@ -17,35 +22,48 @@ const NavBar = () => {
 
     if (isError) return toast.error(`Error: ${error.data?.message}`);
 
-    return (
-        <section
-            className="w-80 flex justify-between items-center"
-            id="nav-button_group"
-        >
+    if (!pathname.endsWith("/dashboard")) {
+        dashboardLink = (
             <Link
                 to="/dashboard"
                 id="dashboard_btn"
                 aria-label="navigate to the dashboard"
-                className="hover:text-brightPurple"
+                className="hover:text-brightPurple mx-2"
             >
                 Dashboard
             </Link>
+        );
+    }
+    if (!pathname.endsWith("/profile")) {
+        profileLink = (
             <Link
                 to="/profile"
                 id="dashboard_btn"
                 aria-label="navigate to your profile page"
-                className="hover:text-brightPurple"
+                className="hover:text-brightPurple mx-2"
             >
                 Profile
             </Link>
+        );
+    }
+    if (!pathname.endsWith("/dashboard/newTask")) {
+        addTaskLink = (
             <Link
                 to="/dashboard/newTask"
                 id="addTask_btn"
                 aria-label="navigate to the add new task form"
-                className="hover:text-brightPurple"
+                className="hover:text-brightPurple mx-2"
             >
                 Add a task
             </Link>
+        );
+    }
+
+    return (
+        <section className="flex justify-between items-center w-64" id="nav-button_group">
+            {dashboardLink}
+            {profileLink}
+            {addTaskLink}
             <button
                 onClick={logoutClicked}
                 id="logout_btn"
